b96b7b48ac135ac00bb1efce60d2f02369491f87
Kirill Bolotov <k.y.bolotov@gmail.com>
2022-11-30 23:36:11 +0700

    Refactored blas-optimization

---
+++ lab7_default/MatrixBLAS.h
@@ -0,0 +1,45 @@
+#ifndef LAB7_MATRIXBLAS_H
+#define LAB7_MATRIXBLAS_H
+#include <iostream>
+#include <chrono>
+#include <valarray>
+#include "mkl_cblas.h"
+
+class Matrix {
+private:
+    int matrixSize_;
+    float* matrix_;
+    float firstNorm_;
+    float infinityNorm_;
+    template<typename Type>
+    void swap(Type& first, Type& second);
+public:
+    explicit Matrix(int matrixSize);
+    Matrix(const Matrix& origin);
+    void setIdentityMatrix();
+    void setZeroMatrix();
+    void setMatrix();
+    Matrix transposeMatrix();
+    void findMatrixNorms();
+    int getMatrixSize() const {return matrixSize_;}
+    float getFirstNorm() const {return firstNorm_;}
+    float getInfinityNorm() const {return infinityNorm_;}
+    void printMatrix();
+    void printMatrix() const;
+    bool operator==(const Matrix& other);
+    Matrix& operator=(const Matrix& other);
+    Matrix& operator-=(const Matrix& other);
+    Matrix& operator+=(const Matrix& other);
+    Matrix& operator*=(const Matrix& other);
+    Matrix& operator/=(float coefficient);
+    ~Matrix();
+};
+
+Matrix operator-(const Matrix& first, const Matrix& second);
+Matrix operator+(const Matrix& first, const Matrix& second);
+Matrix operator*(const Matrix& first, const Matrix& second);
+Matrix FindTransformedMatrix(Matrix& generalMatrix);
+Matrix FindMatrixOfSeries(Matrix& generalMatrix, Matrix& transformedMatrix);
+Matrix FindInverseMatrixAlgorithm(Matrix& generalMatrix, int numberOfIterations);
+
+#endif //LAB7_MATRIXBLAS_H
