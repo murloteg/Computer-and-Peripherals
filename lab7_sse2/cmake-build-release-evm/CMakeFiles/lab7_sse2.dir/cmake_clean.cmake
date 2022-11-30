296eab2ca3e8a17fd1693ebe1b41ed5314e75344
Kirill Bolotov <k.y.bolotov@gmail.com>
2022-11-30 23:54:30 +0700

    Added lab7 with SSE2 optimization

---
+++ lab7_sse2/cmake-build-release-evm/CMakeFiles/lab7_sse2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab7_sse2.dir/SSE2_vectorization.cpp.o"
+  "lab7_sse2"
+  "lab7_sse2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab7_sse2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
